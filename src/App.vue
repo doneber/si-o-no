b966d9150c8665381e3de72f672813fc46bb0dca
Eber <doneber98@gmail.com>
2023-05-18 17:19:28 -0400

    Add csspico

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <p>
-    ¡Empezamos!
-  </p>
+  <header class="container">
+    <h1>Voting System</h1>
+  </header>
+  <main class="container">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quae mollitia libero iusto earum accusantium, ad
+      voluptatum aliquam voluptatibus, non consequuntur blanditiis dicta tempora enim distinctio hic facilis iure! Sit!
+    </p>
+  </main>
 </template>
