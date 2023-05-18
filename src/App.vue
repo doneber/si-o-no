dea0fe48c9ef3c22c5fb6fa4728aec1f5104529c
Eber <doneber98@gmail.com>
2023-05-18 17:40:13 -0400

    Add main view to go to the room

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const username = ref('')
+
+const createRoom = () => {
+  console.log('createRoom')
+}
+
+const joinRoom = () => {
+  console.log('joinRoom')
+}
+
+</script>
+
 <template>
   <header class="container">
     <h1>Voting System</h1>
+    <p>Empieza a votar de una forma rápida y sencilla</p>
   </header>
   <main class="container">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quae mollitia libero iusto earum accusantium, ad
-      voluptatum aliquam voluptatibus, non consequuntur blanditiis dicta tempora enim distinctio hic facilis iure! Sit!
-    </p>
+    <div class="form">
+      <label>Nombre de usuario
+        <input v-model="username" type="text" placeholder="Ej. Juan" required />
+      </label>
+      <div class="grid">
+        <button @click="createRoom()" class="primary" type="submit">Crear sala</button>
+        <button @click="joinRoom()" class="secondary" type="submit">Unirse a una sala</button>
+      </div>
+    </div>
   </main>
 </template>
