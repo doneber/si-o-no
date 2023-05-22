f05486a347b6922d4c2a07446042e484d7d46830
Eber <doneber98@gmail.com>
2023-05-22 19:49:32 -0400

    Add redirections in respective cases

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,49 @@
 <script setup>
-import Home from './views/Home.vue'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { useUserStore } from './stores/userStore'
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import config from '../config.js';
+
+onMounted(async () => {
+  const userStore = useUserStore()
+  const { name, roomId } = userStore
+
+  const firebaseConfig = config.firebaseConfig
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  const router = await useRouter();
+  const { currentRoute } = await router;
+  const { path } = await currentRoute.value;
+  const isValidRoomId = async (idRoom) => {
+    if (idRoom) {
+      const roomDocRef = await doc(db, 'rooms', idRoom)
+      const roomDocSnap = await getDoc(roomDocRef)
+      return roomDocSnap.exists() ? true : false
+    }
+    return false
+  }
+
+  if (path === '/') {
+    if (await isValidRoomId(roomId)) {
+      router.replace(`/room/${roomId}`);
+    } else {
+      router.replace('/');
+    }
+  } else if (path === '/join') {
+    if (await isValidRoomId(roomId)) {
+      router.replace(`/room/${roomId}`);
+    } else {
+      router.replace('/');
+    }
+  } else if (path.startsWith('/room/')) {
+    if (!name || await !isValidRoomId(roomId)) {
+      router.replace('/');
+    }
+  }
+})
 </script>
 
 <template>
